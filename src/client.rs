d9e7837356dd0bf0dad463279fe303146de08299
Michael Smythe <michael.smythe@outlook.com>
2023-12-27 00:50:32 -0500

    Worked on basic NBT implememtnations
    Re-exported BinWrite trait for NBT for use in SMB crate

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,17 @@
-use std::io::{Read, Write};
+// Std
+//use std::io::{Read, Write};
 use std::net::{IpAddr, TcpStream};
 
-use binrw::BinRead;
-use binrw::{io::BufReader, io::Cursor, io::NoSeek, BinWrite};
+// 3rd Party
+use binrw::BinWrite;
+use binrw::{io::BufReader, io::Cursor, io::NoSeek};
 
-use crate::v1::message::cmd::NegotiateResponse;
+// Protocol Crates
+use nbt::nbt::*;
+use nbt::BinWrite as NbtBinWrite;
+
+// Internal
+//use crate::v1::message::cmd::NegotiateResponse;
 use crate::v1::message::Message;
 
 pub struct Client {
@@ -28,8 +35,8 @@ impl Client {
 
     pub fn connect(&mut self, _share: &str) {
         // TODO: Make a stream split method. Should return a writer and a reader
-        self.tcp_connect();
-        let stream = self.stream();
+        //self.tcp_connect();
+        //let stream = self.stream();
 
         // Create a message buffer
         //let mut message = Cursor::new(Vec::new());
@@ -49,26 +56,40 @@ impl Client {
         //let response: NegotiateResponse = Message::from(netbios.message).try_into().unwrap();
 
         // TODO: Fix this to be wrapped in a netbios message
-        stream.write_all(&vec![0x00, 0x00, 0x00, 0x61]).unwrap();
 
         // TODO: Use a persistent write buffer
         // NOTE: Need to figure out a good way to do a buffer that
         // does not continually grow
-        let mut buffer = Cursor::new(Vec::new());
-        Message::negotiate().write(&mut buffer).unwrap();
-        println!("MESSAGE SIZE: {}", buffer.get_ref().len());
+        let mut smb_buf = Cursor::new(Vec::new());
+        Message::negotiate().write(&mut smb_buf).unwrap();
+        println!("MESSAGE SIZE: {}", smb_buf.get_ref().len());
 
-        stream.write_all(&buffer.into_inner()).unwrap();
+        let session = SessionBuilder::default()
+            .packet_type(PacketType::Message)
+            .flags(Flags::new().with_reserved_checked(0).unwrap())
+            .payload(smb_buf.into_inner())
+            .build()
+            .unwrap();
+
+        let mut nbt_buf = Cursor::new(Vec::new());
+        session.write(&mut nbt_buf).unwrap();
+        println!("{:?}", nbt_buf.into_inner());
+        println!("{:02x?}", session.as_bytes());
+
+        //println!("SESSION MESSAGE TYPE: {:?}", session.packet_type);
+        //println!("SESSION FLAGS: {:?}", session.flags);
+        //println!("SESSION LENGTH: {:#02x}", session.length);
+        //println!("SESSION PAYLOAD: {:?}", session.payload);
 
         // Accept the netbios header
-        let mut buffer = [0u8; 4];
-        stream.read_exact(&mut buffer).unwrap();
+        //let mut buffer = [0u8; 4];
+        //stream.read_exact(&mut buffer).unwrap();
 
         // Accept the negotiate response
-        let mut reader = BufReader::new(stream);
-        let message: NegotiateResponse = Message::read(&mut reader).unwrap().try_into().unwrap();
+        //let mut reader = BufReader::new(stream);
+        //let message: NegotiateResponse = Message::read(&mut reader).unwrap().try_into().unwrap();
 
-        println!("{:#?}", message);
+        //println!("{:#?}", message);
     }
 
     fn stream(&mut self) -> &mut NoSeek<TcpStream> {
