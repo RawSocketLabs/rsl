57ba39339739f7a2d6d2208a337ffcd2833ecba7
Michael Benedetti <michael.d.benedetti@gmail.com>
2025-08-06 20:58:48 -0500

    Exposed several more methods on DSDDecoder

--- rust-dsdcc/src/lib.rs
+++ rust-dsdcc/src/lib.rs
@@ -1,5 +1,17 @@
+use std::fmt;
+
+use crate::ffi::{DSDStationType, DSDSyncType};
+
 #[cxx::bridge()]
 pub mod ffi {
+    #[repr(u32)]
+    #[derive(Debug)]
+    pub enum DSDStationType {
+        DSDStationTypeNotApplicable,
+        DSDBaseStation,
+        DSDMobileStation
+    }
+
     #[repr(u32)]
     pub enum DSDDecodeMode {
         DSDDecodeAuto,
@@ -15,15 +27,54 @@ pub mod ffi {
         DSDDecodeYSF,
     }
 
+    #[repr(u32)]
+    #[derive(Debug)]
+    pub enum DSDSyncType {
+        DSDSyncP25p1P,
+        DSDSyncP25p1N,
+        DSDSyncX2TDMADataP,
+        DSDSyncX2TDMAVoiceN,
+        DSDSyncX2TDMAVoiceP,
+        DSDSyncX2TDMADataN,
+        DSDSyncDStarP,
+        DSDSyncDStarN,
+        DSDSyncNXDNP,
+        DSDSyncNXDNN,
+        DSDSyncDMRDataP,
+        DSDSyncDMRDataMS,
+        DSDSyncDMRVoiceP,
+        DSDSyncDMRVoiceMS,
+        DSDSyncProVoiceP,
+        DSDSyncProVoiceN,
+        DSDSyncNXDNDataP,
+        DSDSyncNXDNDataN,
+        DSDSyncDStarHeaderP,
+        DSDSyncDStarHeaderN,
+        DSDSyncDPMR,
+        DSDSyncDPMRPacket,
+        DSDSyncDPMRPayload,
+        DSDSyncDPMREnd,
+        DSDSyncYSF,
+        DSDSyncNone,
+    }
+
     unsafe extern "C++" {
         include!("rust_dsdcc.h");
 
         type DSDDecoder;
         type DSDDecodeMode;
+        type DSDSyncType;
+        type DSDStationType;
 
         fn run(self: &DSDDecoder, sample: i16);
         fn setQuiet(self: &DSDDecoder);
         fn setDecodeMode(self: &DSDDecoder, mode: DSDDecodeMode, on: bool);
+        fn getSlot0Text(self: &DSDDecoder) -> String;
+        fn getSlot1Text(self: &DSDDecoder) -> String;
+        fn getSyncType(self: &DSDDecoder) -> DSDSyncType;
+        fn getFrameTypeText(self: &DSDDecoder) -> String;
+        fn getFrameSubtypeText(self: &DSDDecoder) -> String;
+        fn getStationType(self: &DSDDecoder) -> DSDStationType;
 
         fn create_dsddecoder() -> UniquePtr<DSDDecoder>;
     }
@@ -32,7 +83,6 @@ pub mod ffi {
 pub struct DSDDecoder {
     internal: cxx::UniquePtr<ffi::DSDDecoder>,
 }
-
 
 impl DSDDecoder {
     pub fn new() -> Self {
@@ -51,5 +101,41 @@ impl DSDDecoder {
 
     pub fn set_decode_mode(&self, mode: ffi::DSDDecodeMode, on: bool) {
         self.internal.setDecodeMode(mode, on);
+    }
+
+    pub fn get_slot_0_text(&self) -> String {
+        self.internal.getSlot0Text()
+    }
+
+    pub fn get_slot_1_text(&self) -> String {
+        self.internal.getSlot1Text()
+    }
+
+    pub fn get_sync_type(&self) -> ffi::DSDSyncType {
+        self.internal.getSyncType()
+    }
+
+    pub fn get_frame_type_text(&self) -> String {
+        self.internal.getFrameTypeText()
+    }
+
+    pub fn get_frame_subtype_text(&self) -> String {
+        self.internal.getFrameSubtypeText()
+    }
+
+    pub fn get_station_type(&self) -> ffi::DSDStationType {
+        self.internal.getStationType()
+    }
+}
+
+impl fmt::Display for DSDSyncType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl fmt::Display for DSDStationType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
     }
 }
--- rust-dsdcc/src/rust_dsdcc.h
+++ rust-dsdcc/src/rust_dsdcc.h
@@ -4,8 +4,11 @@
 #include "rust/cxx.h"
 #include <cstdint>
 #include <memory>
+#include <string>
 
 typedef DSDcc::DSDDecoder::DSDDecodeMode DSDDecodeMode;
+typedef DSDcc::DSDDecoder::DSDSyncType DSDSyncType;
+typedef DSDcc::DSDDecoder::DSDStationType DSDStationType;
 
 class DSDDecoder {
 public:
@@ -14,8 +17,15 @@ public:
   void run(short sample) const;
   void setQuiet() const;
   void setDecodeMode(DSDDecodeMode mode, bool on) const;
+  rust::String getSlot0Text() const;
+  rust::String getSlot1Text() const;
+  rust::String getFrameTypeText() const;
+  rust::String getFrameSubtypeText() const;
+  DSDcc::DSDDecoder::DSDSyncType getSyncType() const;
+  DSDcc::DSDDecoder::DSDStationType getStationType() const;
+
   
-  const std::unique_ptr<::DSDcc::DSDDecoder> dsddecoder;
+  std::unique_ptr<::DSDcc::DSDDecoder> dsddecoder;
 };
 
 std::unique_ptr<::DSDDecoder> create_dsddecoder();
